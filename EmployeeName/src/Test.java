a2ecf6f69f943810d2a76e9eb687c578214260c6
Durga Prasad <Durga Prasad@LAPTOP-I0RGD3A8>
2021-10-02 20:56:26 +0530

    code

---
+++ EmployeeName/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	int eid=101;
+	String ename="durga";
+	String eaddrs="hyderabad";
+	public static void main(String[] args) {
+		Test t=new Test();
+		System.out.println( t.eid);
+		System.out.println( t.ename);
+		System.out.println( t.eaddrs);
+	}
+	
+	
+}
